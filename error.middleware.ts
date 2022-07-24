d6d547e58980cc04f136b0d45216909c60924f1e
Maxim Bogochas <bogochunas@gmail.com>
2022-07-24 14:25:33 +0300

    fix: fix "loger" typo

--- error.middleware.ts
+++ error.middleware.ts
@@ -1,29 +1,40 @@
 import { Request, Response, NextFunction } from 'express';
 import { BaseException } from './errors/base.exception.js';
 import bunyan from 'bunyan';
+import Logger from 'bunyan';
 
 const logger = bunyan.createLogger({ name: process.env.npm_package_name as string });
 
+declare interface LoggerRequest extends Request {
+    logger: Logger
+}
+
+declare interface LoggerResponse extends Response {
+    sentry: string;
+}
+
+declare interface ResponseBody {
+    status: string;
+    message: string;
+    error_id?: string;
+}
+
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-export function ErrorMiddleware (error: Error, req :Request, resp: Response, next: NextFunction) {
+export function ErrorMiddleware (error: Error, req :LoggerRequest, resp: LoggerResponse, next: NextFunction) {
     let code;
-    let body;
+    let body: ResponseBody;
 
     if (error instanceof BaseException) {
-        // @ts-ignore
-        (req.loger ? req.loger : logger).error(error);
+        (req.logger ? req.logger : logger).error(error);
         code = error.code;
         body = { status: 'error', message: error.message };
     } else {
-        // @ts-ignore
-        (req.loger ? req.loger : logger).error(error);
+        (req.logger ? req.logger : logger).error(error);
         code = 500;
         body = { status: 'error', message: 'Internal error' };
     }
 
-    // @ts-ignore
     if (resp.sentry) {
-        // @ts-ignore
         body.error_id = resp.sentry;
     }
 
