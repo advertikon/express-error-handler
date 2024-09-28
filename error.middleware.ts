d60ed8a22499dae640c953a69eb83a9b79e26650
Maxim Bogochunas <bogochunas@gmail.com>
2024-09-28 17:09:56 +0300

    feat: do not catch unhandled errors
    BREAKING CHANGE: unhandled errors not hadled by main middleware

--- error.middleware.ts
+++ error.middleware.ts
@@ -1,56 +1,68 @@
-import { NextFunction, Response } from 'express';
-import bunyan from 'bunyan';
-import { PathParams } from 'express-serve-static-core';
-import VError from 'verror';
-import { ulid } from 'ulid';
-import { HTTP_ERROR } from './error.js';
-import { LoggerRequest } from './types.js';
-import { ResponseBody } from './types.js';
+import { NextFunction, Response } from "express";
+import bunyan from "bunyan";
+import { PathParams } from "express-serve-static-core";
+import VError from "verror";
+import { ulid } from "ulid";
+import { HTTP_ERROR } from "./error.js";
+import { LoggerRequest } from "./types.js";
+import { ResponseBody } from "./types.js";
 
-const logger = bunyan.createLogger({ name: process.env.npm_package_name as string });
+const logger = bunyan.createLogger({
+  name: process.env.npm_package_name as string,
+});
 
 type Options = {
-    nonLogableExceptions: HTTP_ERROR[];
-    defaultErrorMessage: string;
-}
+  nonLogableExceptions: HTTP_ERROR[];
+  defaultErrorMessage: string;
+};
 
-export function ErrorMiddleware (options: Options = {
+export function ErrorMiddleware(
+  options: Options = {
     nonLogableExceptions: [
-        HTTP_ERROR.FORBIDDEN,
-        HTTP_ERROR.UNAUTHORIZED,
-        HTTP_ERROR.VALIDATION,
+      HTTP_ERROR.FORBIDDEN,
+      HTTP_ERROR.UNAUTHORIZED,
+      HTTP_ERROR.VALIDATION,
     ],
-    defaultErrorMessage: ''
- }) {
-    return function (
-        error: Error|VError,
-        req :LoggerRequest,
-        resp: Response,
-        next: NextFunction
-    ) {
-        const errorTrackingCode = req.req_id || ulid();
-        let code = 500;
-        let logable = true;
+    defaultErrorMessage: "",
+  }
+) {
+  return function (
+    error: Error | VError,
+    req: LoggerRequest,
+    resp: Response,
+    next: NextFunction
+  ) {
+    if (error.name === "Error") {
+      next(error);
+      return;
+    }
 
-        const body = {
-            status: 'error',
-            code,
-            message: 'Internal server error'
-        } as ResponseBody;
+    const errorTrackingCode = req.req_id || ulid();
+    let code = 500;
+    let logable = true;
 
-        if (error.constructor.name === 'VError') {
-            code = VError.info(error).code;
-            body.message = options.defaultErrorMessage || error.message;
-            body.code = code;
-            logable = !options.nonLogableExceptions.includes(error.name as HTTP_ERROR);
-        }
+    const body = {
+      status: "error",
+      code,
+      message: "Internal server error",
+    } as ResponseBody;
 
-        if (logable) {
-            (req.logger ? req.logger : logger).error(error, 'Error', { errorCode: errorTrackingCode });
-            body.errorTrackingCode = errorTrackingCode;
-        }
+    if (error.constructor.name === "VError") {
+      code = VError.info(error).code;
+      body.message = options.defaultErrorMessage || error.message;
+      body.code = code;
+      logable = !options.nonLogableExceptions.includes(
+        error.name as HTTP_ERROR
+      );
+    }
 
-        resp.status(code).json(body);
-    } as unknown as PathParams
+    if (logable) {
+      (req.logger ? req.logger : logger).error(error, "Error", {
+        errorCode: errorTrackingCode,
+      });
+      body.errorTrackingCode = errorTrackingCode;
+    }
+
+    resp.status(code).json(body);
+  } as unknown as PathParams;
 }
-
